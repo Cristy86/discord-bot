511a1a6ef9bad987c1f34cb0a4963c1635663a22
Cristy86 <37700326+Cristy86@users.noreply.github.com>
2018-08-07 13:11:00 +0300

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -3,8 +3,7 @@ from discord.ext import commands
 import asyncio
 import traceback
 import discord
-import datetime
-BOT_LAUNCH_TIME = datetime.datetime.utcnow()
+import datetime, random
 
 class Misc:
     """Misc commands."""
@@ -12,13 +11,15 @@ class Misc:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def uptime(self, ctx):
-        delta_uptime = datetime.datetime.utcnow() - BOT_LAUNCH_TIME
-        hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
-        minutes, seconds = divmod(remainder, 60)
-        days, hours = divmod(hours, 24)
-        await ctx.send(f"{days}d, {hours}h, {minutes}m, {seconds}s")
+    @commands.command(name="8ball")
+    async def _ball(self, ctx, *, question:str):
+        """A usual 8ball..."""
+
+        if ctx.author.bot:
+            return
+        
+        results = ["It is certain"," It is decidedly so","Without a doubt","Yes, definitely","You may rely on it","As I see it, yes"," Most likely","Outlook good","Yes","Signs point to yes"," Reply hazy try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"]
+        await ctx.send(f"The 🎱 says: **`{random.choice(results)}.`**")
 
 def setup(bot):
     bot.add_cog(Misc(bot))
